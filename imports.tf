b21b8fd69531b444f52b4c35bb594e6371b7f6ae
dennisvandijk-Git <dennisvandijk91@live.nl>
2024-12-05 14:59:39 +0100

    Del imports.tf

--- imports.tf
+++
@@ -1,4 +0,0 @@
-# import {
-#   id = "/subscriptions/fb6085da-feae-4a43-ae4f-2352d9707ee9/resourceGroups/dev-api-rg/providers/Microsoft.Web/sites/api-linux-function-app/functions/visitor_counter"
-#   to = azurerm_function_app_function.faf
-# }
